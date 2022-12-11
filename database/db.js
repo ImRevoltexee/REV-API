b05cbda1bb2157916e217390abb5bff3ffddc3f6
Revolt <118598395+ImRevoltexee@users.noreply.github.com>
2022-12-11 14:19:10 +0700

    Add files via upload

--- database/db.js
+++ database/db.js
@@ -3,16 +3,16 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.sekha.me';
+var url = 'Url Mongo Db';
 try {
-if(url == 'https://api.sekha.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'URL Mongo Db') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, oke','green'))
+  console.log(color('Connected correctly to server, Revolt','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
